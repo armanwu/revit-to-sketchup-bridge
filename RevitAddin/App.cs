42e164348ab4fd5ce84df9ab25fc4ffde52d2f46
Arman Arisman <arman.wu@gmail.com>
2026-08-17 09:10:16 +0700

    Update icon

--- RevitAddin/App.cs
+++ RevitAddin/App.cs
@@ -1,5 +1,7 @@
-using Autodesk.Revit.UI;
+using System.IO;
 using System.Reflection;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI;
 
 namespace RevitToSketchUpExporter
 {
@@ -13,13 +15,33 @@ namespace RevitToSketchUpExporter
 
             PushButtonData buttonData = new PushButtonData(
                 "ExportToSketchUpBtn",
-                "Export to SketchUp (JSON)",
+                "Export to\nJSON",
                 thisAssemblyPath,
                 "RevitToSketchUpExporter.ExportCommand")
             {
                 ToolTip = "Export the selected elements (or all elements in the active 3D view) to a JSON file " +
                           "that can be read by the 'revit_importer.rb' Ruby script in SketchUp."
             };
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            // Load 128x128 icon for LargeImage (High-DPI sharp rendering)
+            using (Stream streamLarge = assembly.GetManifestResourceStream("RevitToSketchUpExporter.Resources.icon128.png"))
+            {
+                if (streamLarge != null)
+                {
+                    buttonData.LargeImage = BitmapFrame.Create(streamLarge);
+                }
+            }
+
+            // Load 64x64 icon for small Image
+            using (Stream streamSmall = assembly.GetManifestResourceStream("RevitToSketchUpExporter.Resources.icon64.png"))
+            {
+                if (streamSmall != null)
+                {
+                    buttonData.Image = BitmapFrame.Create(streamSmall);
+                }
+            }
 
             panel.AddItem(buttonData);
 
